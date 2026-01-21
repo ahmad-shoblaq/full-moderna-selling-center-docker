4bc1eddf24b03c612789a295af161a296f209387
ahmad-shoblaq <ahmadshoblaq@gmail.com>
2026-01-22 01:37:58 +0200

    Changes to be committed: 	modified:   src/db.php

--- src/db.php
+++ src/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = getenv('DB_HOST') ?: 'db';
 $db   = getenv('DB_NAME') ?: 'selling_center';
-$user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASS') ?: 'rootpass';
+$user = getenv('DB_USER') ?: 'appuser';
+$pass = getenv('DB_PASS') ?: 'apppass';
 
 $conn = new mysqli($host, $user, $pass, $db);
 
